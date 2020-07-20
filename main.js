d36643a95f380edb8b2304f8e0f2e68b3c13e088
Wendell <wendell.pattonjr@gmail.com>
2020-07-20 18:52:14 -0500

    tested data and primitive types

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("test");
+// * undefined
+/* let x;
+consolelog(x); */
+
+// * Boolean
+/* let x = true;
+let y = false;
+
+console.log (x, y);
+ 
+if (y) {
+    console.log("This is true");
+} else {
+    console.log("This is false");
+}
+ */
+
+ // * Number
+
+ /* let number = 22
+
+ console.log(Number(number) * 22); */
+
+ // * String
+
+ let string = "Today is Monday!"
+
+ console.log(typeof string);
+
+ if (string == "Today is Monday!") {
+     console.log("YAYAAA!!!!");
+ }
